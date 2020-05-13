2c125339cb6eb03e4a756f15c0221bfbf85a4c50
insanejohnny <41359461+insanejohnny@users.noreply.github.com>
2020-05-13 10:48:17 +0800

    Add files via upload

--- lianjia/parser/arealist_test.go
+++ lianjia/parser/arealist_test.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"io/ioutil"
 	"testing"
 )
@@ -13,6 +12,33 @@ func TestParseAreaList(t *testing.T) {
 	}
 
 	//TODO: finish test
-	// result := ParseAreaList(contents)
-	fmt.Println(string(contents))
+	result := ParseAreaList(contents)
+
+	const resultSize = 17
+	expectedUrls := []string{
+		"https://sh.lianjia.com/ershoufang/pudong/",
+		"https://sh.lianjia.com/ershoufang/minhang/",
+		"https://sh.lianjia.com/ershoufang/baoshan/",
+	}
+	expectedAreas := []string{
+		"Area 浦东", "Area 闵行", "Area 宝山",
+	}
+
+	if len(result.Requests) != resultSize {
+		t.Errorf("result should have %d requests; but had %d", resultSize, len(result.Requests))
+	}
+	for i, url := range expectedUrls {
+		if result.Requests[i].Url != url {
+			t.Errorf("expected url #%d: %s, but was %s", i, url, result.Requests[i].Url)
+		}
+	}
+
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d requests; but had %d", resultSize, len(result.Items))
+	}
+	for i, area := range expectedAreas {
+		if result.Items[i] != area {
+			t.Errorf("expected area #%d: %s, but was %s", i, area, result.Items[i])
+		}
+	}
 }
